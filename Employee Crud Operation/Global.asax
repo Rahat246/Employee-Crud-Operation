70fdff610f1136bdfb2142716065baf5adad9314
Rahat <Rahat@DESKTOP-6THHEAI>
2024-07-10 16:24:00 +0600

    Add project files.

---
+++ Employee Crud Operation/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Employee_Crud_Operation.MvcApplication" Language="C#" %>
---
+++ YourDbContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Entity;
+
+namespace Employee_Crud_Operation.Models
+{
+    public class YourDbContext : DbContext
+    {
+        public DbSet<employee> employees { get; set; }
+    }
+}
